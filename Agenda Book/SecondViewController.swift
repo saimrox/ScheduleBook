f5d1acf1036eec4b5c1d857e11e7f3e7cd6fc55e
Goodman Murphy-Smith <goodman@goodmans-macbook-pro.local>
2016-10-08 17:33:57 -0500

    Initial Commit

---
+++ Agenda Book/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Agenda Book
+//
+//  Created by Goodman Murphy-Smith on 10/8/16.
+//  Copyright © 2016 Goodman Murphy-Smith. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Agenda BookUITests/Agenda_BookUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Agenda_BookUITests.swift
+//  Agenda BookUITests
+//
+//  Created by Goodman Murphy-Smith on 10/8/16.
+//  Copyright © 2016 Goodman Murphy-Smith. All rights reserved.
+//
+
+import XCTest
+
+class Agenda_BookUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
